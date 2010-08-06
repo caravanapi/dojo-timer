f1ab3022ee313149433b272b14294ac399b6b34e
Cairo Noleto <caironoleto@gmail.com>
2010-08-06 19:20:13 -0300

    Buscando as informações no twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
   validates_uniqueness_of :twitter
   validates_presence_of :twitter
+  before_save :check_on_twitter
+  
+  def check_on_twitter
+    fetch_twitter
+  end
+  
+  def avatar_url
+    fetch_twitter['profile_image_url']
+  end
+  
+  private
+  def fetch_twitter
+    @data ||= Twitter.user(twitter)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 describe User do
+
+  before(:each) do
+    @twitter = Twitter.stub!( :user ) { |username| raise Twitter::NotFound unless username == "caironoleto" } 
+  end
+  
   subject do
     User.new(:twitter => "caironoleto")
   end
+
   it { should be_valid }
 
   it "should not be valid without twitter" do
@@ -15,5 +21,15 @@ describe User do
     subject.save
     user = User.new(:twitter => "caironoleto")
     user.should_not be_valid
+  end
+  
+  it "should check on twitter before save" do
+    lambda { subject.save }.should_not raise_error(Twitter::NotFound)
+    lambda { User.create(:twitter => "caironoleto000") }.should raise_error(Twitter::NotFound)
+  end
+  
+  it "should respond to avatar_url" do
+    Twitter.should_receive(:user).with("caironoleto").and_return({'profile_image_url' => "http://twitter.com/avatars/caironoleto.jpg"})
+    subject.avatar_url.should == "http://twitter.com/avatars/caironoleto.jpg"
   end
 end
