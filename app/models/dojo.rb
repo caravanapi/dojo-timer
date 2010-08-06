db0cd68dadd72eb5f89bde0d3e48edf9f94a4b4e
Cairo Noleto <caironoleto@gmail.com>
2010-08-06 19:19:55 -0300

    Dojo tem vários usuários

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -1,3 +1,4 @@
 class Dojo < ActiveRecord::Base
   validates_presence_of :name
+  has_and_belongs_to_many :users
 end
--- spec/models/dojo_spec.rb
+++ spec/models/dojo_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe Dojo do
+  let(:user) do
+    User.new(:twitter => 'caironoleto')
+  end
+
   subject do
-    Dojo.new(:name => "Oxente Rails")
+    Dojo.new(:name => "Oxente Rails", :users => [user])
   end
 
   it "should be valid with valid attributes" do
     subject.should be_valid
   end
-  
+
   it "should not be valid without name" do
     subject.name = nil
     subject.should_not be_valid
+  end
+
+  it "should has many users" do
+    subject.users.should == [user]
   end
 end
