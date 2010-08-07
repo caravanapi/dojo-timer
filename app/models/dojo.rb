474865f43016f61c9816bc135765bb542b93657e
Dmitry Rocha <dmitry.nix@gmail.com>
2010-08-07 12:22:20 -0300

    adicionado campo para twitter.

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -1,4 +1,12 @@
 class Dojo < ActiveRecord::Base
   validates_presence_of :name
   has_and_belongs_to_many :users
+
+  def user_twitters=(twitters)
+    self.users = twitters.split(',').map{ |twitter| User.find_or_create_by_twitter(twitter.lstrip) }
+  end
+
+  def user_twitters
+    users.map{ |user| user.twitter }
+  end
 end
